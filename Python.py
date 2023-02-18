5e73ba38a6fad019fe75c2c3d2ea90f8d845070e
isinare <91046353+isinare@users.noreply.github.com>
2023-02-18 14:16:28 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,2 +1,2 @@
-//Hello, I am Ishan Sinare, online alias ZladeTheKing, i have learnt python and java language to some extent and here i present some of the things that i have done
+#Hello, I am Ishan Sinare, online alias ZladeTheKing, i have learnt python and java language to some extent and here i present some of the things that i have done
 print("hello world")
